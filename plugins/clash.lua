5f8f89ac5cd177f8091e188953379793b1503275
pekkahacker <pekkahacker@gmail.com>
2016-05-31 21:13:27 +0430

    fix clash plugin

--- plugins/clash.lua
+++ plugins/clash.lua
@@ -4,105 +4,105 @@ local apikey =
 
 local function run(msg, matches)
 
- if matches[1]:lower() == 'clash' then
+ if matches[1]:lower() == 'clash' then
 
-  local clantag = matches[2]
+  local clantag = matches[2]
 
-  if string.match(matches[2], '^#.+$') then
+  if string.match(matches[2], '^#.+$') then
 
-     clantag = string.gsub(matches[2], '#', '')
+     clantag = string.gsub(matches[2], '#', '')
 
-  end
+  end
 
-  clantag = string.upper(clantag)
+  clantag = string.upper(clantag)
 
-  local curl = 'curl -X GET --header "Accept: application/json" --header "authorization: Bearer '..apikey..'" "https://api.clashofclans.com/v1/clans/%23'..clantag..'"'
+  local curl = 'curl -X GET --header "Accept: application/json" --header "authorization: Bearer '..apikey..'" "https://api.clashofclans.com/v1/clans/%23'..clantag..'"'
 
-  cmd = io.popen(curl)
+  cmd = io.popen(curl)
 
-  
+  
 
-  local result = cmd:read('*all')
+  local result = cmd:read('*all')
 
-  local jdat = json:decode(result)
+  local jdat = json:decode(result)
 
 if jdat.reason then
 
-      if jdat.reason == 'accessDenied' then return 'برای ثبت API Key خود به سایت زیر بروید\ndeveloper.clashofclans.com' end
+      if jdat.reason == 'accessDenied' then return 'برای ثبت API Key خود به سایت زیر بروید\ndeveloper.clashofclans.com' end
 
-   return '#Error\n'..jdat.reason
+   return '#Error\n'..jdat.reason
 
-  end
+  end
 
-  local text = 'Tag: '.. jdat.tag
+  local text = 'Tag: '.. jdat.tag
 
-     text = text..'\nName: '.. jdat.name
+     text = text..'\nName: '.. jdat.name
 
-     text = text..'\ntype: '.. jdat.type
+     text = text..'\ntype: '.. jdat.type
 
-     text = text..'\nWar Frequency: '.. jdat.warFrequency
+     text = text..'\nWar Frequency: '.. jdat.warFrequency
 
-     text = text..'\nLocation: '..jdat.location.name
+     text = text..'\nLocation: '..jdat.location.name
 
-     text = text..'\nClan Level: '.. jdat.clanLevel
+     text = text..'\nClan Level: '.. jdat.clanLevel
 
-     text = text..'\nWar wins: '.. jdat.warWins
+     text = text..'\nWar wins: '.. jdat.warWins
 
-     text = text..'\nClan points: '.. jdat.clanPoints
+     text = text..'\nClan points: '.. jdat.clanPoints
 
-     text = text..'\nRequired Trophies: '.. jdat.requiredTrophies
+     text = text..'\nRequired Trophies: '.. jdat.requiredTrophies
 
-     text = text..'\nMembers: '.. jdat.members..'نفر' 
+     text = text..'\nMembers: '.. jdat.members..'نفر' 
 
   text = text..'\nDescription: '.. jdat.description
 
-     cmd:close()
+     cmd:close()
 
-  return text
+  return text
 
- end
+ end
 
- if matches[1]:lower() == 'clash>' then
+ if matches[1]:lower() == 'clash>' then
 
-  local members = matches[2]
+  local members = matches[2]
 
-  if string.match(matches[2], '^#.+$') then
+  if string.match(matches[2], '^#.+$') then
 
-     members = string.gsub(matches[2], '#', '')
+     members = string.gsub(matches[2], '#', '')
 
-  end
+  end
 
-  members = string.upper(members)
+  members = string.upper(members)
 
-  local curl = 'curl -X GET --header "Accept: application/json" --header "authorization: Bearer '..apikey..'" "https://api.clashofclans.com/v1/clans/%23'..members..'/members"'
+  local curl = 'curl -X GET --header "Accept: application/json" --header "authorization: Bearer '..apikey..'" "https://api.clashofclans.com/v1/clans/%23'..members..'/members"'
 
-  cmd = io.popen(curl)
+  cmd = io.popen(curl)
 
-  local result = cmd:read('*all')
+  local result = cmd:read('*all')
 
-  local jdat = json:decode(result)
+  local jdat = json:decode(result)
 
-  if jdat.reason then
+  if jdat.reason then
 
-      if jdat.reason == 'accessDenied' then return 'برای ثبت API Key خود به سایت زیر بروید\ndeveloper.clashofclans.com' end
+   if jdat.reason == 'accessDenied' then return 'برای ثبت API Key خود به سایت زیر بروید\ndeveloper.clashofclans.com' end
 
-   return '#Error\n'..jdat.reason
+  return '#Error\n'..jdat.reason
 
-  end
+  end
 
-  local leader = ""
+  local leader = ""
 
-  local coleader = ""
+  local coleader = ""
 
-  local items = jdat.items
+  local items = jdat.items
 
-  leader = 'edlers: \n'
+  leader = 'edlers: \n'
 
-   for i = 1, #items do
+   for i = 1, #items do
 
-   if items[i].role == "leader" then
+   if items[i].role == "leader" then
 
-   leader = leader.."\nLeader: "..items[i].name.."\nLevel: "..items[i].expLevel.."..items[i].league.name
+   leader = leader.."\nLeader: "..items[i].name.."\nLevel: "..items[i].expLevel.."..items[i].league.name"
 
 end
 
